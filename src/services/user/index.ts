27f50b946c6618597b1a0c21ad63d6112776aee0
Lidia Kovac <lidia.kovac1998@gmail.com>
2023-08-02 19:33:56 +0200

    fix

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -10,146 +10,154 @@ import multer from "multer"
 import { ObjectId } from "mongodb"
 //admin middleware
 userRoute.get(
-    "/",
-    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-        try {
-            const allCharacters = await User.find()
-            res.send(allCharacters)
-        } catch (e) {
-            next(e)
-        }
-    }
-)
-userRoute.get(
-    "/me",
-    authMidd,
-    async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            res.send(req.user)
-        } catch (error) {
-            next(error)
-        }
-    }
-)
-
-userRoute.post(
-    "/login",
-    async ({ body }: Request, res: Response, next: NextFunction) => {
-        try {
-            console.log(body)
-            let foundUser = await User.findOne({
-                where: {
-                    email: body.email,
-                },
-            })
-            if (foundUser && (await compare(body.password, foundUser.password))) {
-                const token = await generateJWT({
-                    lastName: foundUser!.lastName,
-                    email: foundUser!.email,
-                })
-                res
-                    .set("Access-Control-Expose-Headers", "token")
-                    .set("token", token as string)
-                    .send(200)
-            } else res.send(404)
-        } catch (error) {
-            next(error)
-        }
-    }
-)
-
-userRoute.get(
-    "/:id",
-    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-        try {
-            const selectedUser = await User.findById(req.params.id)
-            res.send(selectedUser)
-        } catch (e) {
-            next(e)
-        }
-    }
-)
-
-userRoute.patch("/:id/:courseId", authMidd, async (req: Request, res: Response, next: NextFunction) => {
+  "/",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-        const user = await User.findById(req.params.id).populate("favs")
-        if ((user!.favs as Course[]).some((el: Course) => new ObjectId(el._id).equals(new ObjectId(req.params.courseId)))) {
-            await User.findByIdAndUpdate(req.params.id, {
-                $pullAll: {
-                    favs: [req.params.courseId]
-                }
-            })
-        } else {
-            await User.findByIdAndUpdate(req.params.id, {
-                $push: {
-                    favs: req.params.courseId
-                }
-            })
-        }
-        res.sendStatus(204)
-    } catch (error) {
-        console.log(error)
-        next()
-    }
-})
-
-userRoute.post(
-    "/",
-    multer().fields([
-        { name: "name" },
-        { name: "email" },
-        { name: "password" },
-        { name: "passwordConfirm" },
-    ]),
-    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-        try {
-            if (req.body.password !== req.body.passwordConfirm)
-                res.status(400).send("Passwords don't match!")
-            const checkUnique = await User.findOne({
-                where: {
-                    email: req.body.email
-                }
-            })
-            if (checkUnique) {
-                res.status(400).send("Email is already in our system!")
-            }
-            await User.create({
-                ...req.body,
-
-            })
-            res.status(201).send("User created")
-        } catch (e) {
-            next(e)
-        }
-    }
-)
-
-userRoute.post("/logout", async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-
+      const allCharacters = await User.find()
+      res.send(allCharacters)
     } catch (e) {
-        next(e)
+      next(e)
     }
-})
+  }
+)
+userRoute.get(
+  "/me",
+  authMidd,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.send(req.user)
+    } catch (error) {
+      next(error)
+    }
+  }
+)
 
+userRoute.post(
+  "/login",
+  multer().fields([{ name: "email" }, { name: "password" }]),
+  async ({ body }: Request, res: Response, next: NextFunction) => {
+    try {
+      console.log(body)
+      let foundUser = await User.findOne({
+        
+          email: body.email,
+      })
+      if (foundUser && (await compare(body.password, foundUser.password))) {
+        const token = await generateJWT({
+          lastName: foundUser!.lastName,
+          email: foundUser!.email,
+        })
+        res
+          .set("Access-Control-Expose-Headers", "token")
+          .set("token", token as string)
+          .send(200)
+      } else res.send(404)
+    } catch (error) {
+      next(error)
+    }
+  }
+)
+
+userRoute.get(
+  "/:id",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const selectedUser = await User.findById(req.params.id)
+      res.send(selectedUser)
+    } catch (e) {
+      next(e)
+    }
+  }
+)
+
+userRoute.patch(
+  "/:id/:courseId",
+  authMidd,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const user = await User.findById(req.params.id).populate("favs")
+      if (
+        (user!.favs as Course[]).some((el: Course) =>
+          new ObjectId(el._id).equals(new ObjectId(req.params.courseId))
+        )
+      ) {
+        await User.findByIdAndUpdate(req.params.id, {
+          $pullAll: {
+            favs: [req.params.courseId],
+          },
+        })
+      } else {
+        await User.findByIdAndUpdate(req.params.id, {
+          $push: {
+            favs: req.params.courseId,
+          },
+        })
+      }
+      res.sendStatus(204)
+    } catch (error) {
+      console.log(error)
+      next()
+    }
+  }
+)
+
+userRoute.post(
+  "/",
+  multer().fields([
+    { name: "name" },
+    { name: "email" },
+    { name: "password" },
+    { name: "passwordConfirm" },
+  ]),
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      if (req.body.password !== req.body.passwordConfirm)
+        res.status(400).send("Passwords don't match!")
+      const checkUnique = await User.findOne({
+        where: {
+          email: req.body.email,
+        },
+      })
+      if (checkUnique) {
+        res.status(400).send("Email is already in our system!")
+      }
+      await User.create({
+        ...req.body,
+      })
+      res.status(201).send("User created")
+    } catch (e) {
+      next(e)
+    }
+  }
+)
+
+userRoute.post(
+  "/logout",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+    } catch (e) {
+      next(e)
+    }
+  }
+)
 
 userRoute.put(
-    "/:id",
-    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-        try {
-        } catch (e) {
-            next(e)
-        }
+  "/:id",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+    } catch (e) {
+      next(e)
     }
+  }
 )
 userRoute.delete(
-    "/:id",
-    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-        try {
-        } catch (e) {
-            next(e)
-        }
+  "/:id",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+    } catch (e) {
+      next(e)
     }
+  }
 )
 
 export default userRoute
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,40 +1,42 @@
 import { NextFunction, Request, Response } from "express"
 import { sign, verify } from "jsonwebtoken"
 import User from "../models/user"
-const { JWT_SECRET } = process.env
 export const generateJWT = (payload: JwtPayload) => {
-    return new Promise((res, rej) => sign(payload, JWT_SECRET!, { expiresIn: "1 day" }, (err, token) => {
-        if (err) res(err)
+    return new Promise((res, rej) => sign(payload, process.env.JWT_SECRET!, { expiresIn: "1 day" }, (err, token) => {
+        if (err) rej(err)
         else res(token)
     }))
 }
 
 export const verifyJWT = (token: string): Promise<JwtPayload | Error> => {
-    return new Promise((res, rej) => verify(token, JWT_SECRET!, (err, decoded) => {
+    return new Promise((res, rej) => verify(token, process.env.JWT_SECRET!, (err, decoded) => {
         if (err) res(err)
         else res(decoded as JwtPayload)
     }))
 }
 
 export const authMidd = async (req: Request, res: Response, next: NextFunction) => {
-    if (!req.headers["authorization"]) res.status(401).send("please send token :(")
-    else {
+    try {
+        
+        if (!req.headers["authorization"]) res.status(401).send("please send token :(")
+        else {
         const decoded = await verifyJWT(req.headers["authorization"].replace("Bearer ", "")) as JwtPayload
         if (decoded.exp) {
             //if there is a token (otherwise it's an error)
             delete decoded.iat
             delete decoded.exp
+            console.log(decoded)
             const me = await User.findOne({
-                where: {
-                    ...decoded
-                },
-                attributes: ["email", "name", "lastName"]
-
-            })
-            if (me) {
-                req.user = me
-                next()
-            } else res.status(401).send("user is not found :(")
-        } else res.status(401).send("token expired or wrong")
+                    ...decoded,
+                    
+                })
+                if (me) {
+                    req.user = me
+                    next()
+                } else res.status(401).send("user is not found :(")
+            } else res.status(401).send("token expired or wrong")
+        }
+    } catch (error) {
+       next(error) 
     }
 }
